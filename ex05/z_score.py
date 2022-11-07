5c17a6ebfcac6cd8a7a516242356eba202bb52af
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-07 15:33:30 +0100

    ok

--- ex05/z_score.py
+++ ex05/z_score.py
@@ -16,7 +16,7 @@ def zscore(x):
 
 def main_test():
     X = np.array([0, 15, -9, 7, 12, 3, -21])
-    prin(zscore(X))
+    print(zscore(X))
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
     print(zscore(Y))
 
