c947836629f489896875f7356c1bd2da24d5707f
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-07 15:33:17 +0100

    clean

--- ex05/z_score.py
+++ ex05/z_score.py
@@ -1,20 +1,24 @@
 import numpy as np
 
 def zscore(x):
-    if type(x) != np.ndarray or len(x) == 0:
+    try:
+        if type(x) != np.ndarray or len(x) == 0:
+            return None
+        normalized = np.zeros(x.shape)
+        mean_ = np.mean(x)
+        std_ = np.std(x)
+        for i in range(len(x)):
+            normalized[i] = (x[i] - mean_) / std_
+        return normalized
+    except:
         return None
-    normalized = np.zeros(x.shape)
-    mean_ = np.mean(x)
-    std_ = np.std(x)
-    for i in range(len(x)):
-        normalized[i] = (x[i] - mean_) / std_
-    print(normalized)
+
 
 def main_test():
     X = np.array([0, 15, -9, 7, 12, 3, -21])
-    zscore(X)
+    prin(zscore(X))
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-    zscore(Y)
+    print(zscore(Y))
 
 if __name__ == "__main__":
     main_test()
