0f5dc5a506eb2f8094bebb812a586ea530379399
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-07 15:38:56 +0100

    ex06 ok

---
+++ ex06/minimax.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def minmax(x):
+    try:
+        if type(x) != np.ndarray or len(x) == 0:
+            return None
+        minmaxed = np.zeros(x.shape)
+        min_ = np.min(x)
+        max_ = np.max(x)
+        for i in range(len(x)):
+            minmaxed[i] = (x[i] - min_) / (max_ - min_)
+        return minmaxed
+    except:
+        return None
+
+
+def main_test():
+    X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
+    print(minmax(X))
+    Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+    print(minmax(Y))
+
+if __name__ == "__main__":
+    main_test()
